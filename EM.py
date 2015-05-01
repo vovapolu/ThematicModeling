3b4067622a24444a64ec92f785dbb8ebfd2acc8c
Vladimir Poluhsin <vovapolu@gmail.com>
2015-05-01 18:48:28 +0300

    Refactoring

--- EM.py
+++ EM.py
@@ -6,38 +6,38 @@ from scipy.spatial.distance import euclidean
 import numpy.random.mtrand as mr
 import os.path
 
-def normalized(a):
-    v = sum(a)
-    return a / v
+
+def normalized(v):
+    s = np.sum(v)
+    if s != 0:
+        return v / s
 
 
-def pround(a):
-    frac = a - np.floor(a)
-    if (np.random.random_sample() < 1 - frac):
-        return np.floor(a)
-    else:
-        return np.ceil(a)
+def normalized_matrix_by_columns(m):
+    return np.apply_along_axis(normalized, 0, m)
 
 
-proundVec = np.vectorize(pround)
+def normalized_matrix_by_rows(m):
+    return np.apply_along_axis(normalized, 1, m)
 
 
-def positiveSlice(a):
-    if (a < 0):
-        return 0
-    else:
-        return a
+def pround(v):
+    isRound = np.random.random(size=v.shape) > v - np.floor(v)
+    v[isRound] += 1
+    return np.floor(v).astype(int)
 
 
-positiveSliceVec = np.vectorize(positiveSlice)
+def positive_slice(a):
+    a[a < 0] = 0
+    return a
 
 
-def cutZeroes(m):
-    zeroEps = 0.00001
-    m[np.abs(m) < zeroEps] = 0
+def cut_zeroes(m):
+    zero_eps = 0.00001
+    m[np.abs(m) < zero_eps] = 0
 
 
-def getVersion():
+def get_version():
     try:
         with file("version.txt", "r") as vFile:
             ver = int(vFile.readline())
@@ -46,28 +46,28 @@ def getVersion():
     return ver
 
 
-def incrementVersion():
-    ver = getVersion()
+def increment_version():
+    ver = get_version()
     with file("version.txt", "w") as vFile:
         vFile.write("{0}".format(ver + 1))
     return ver + 1
 
 
-def loadFromFile(filename):
+def load_from_file(filename):
     phi = np.loadtxt(filename + "_phi.txt")
     theta = np.loadtxt(filename + "_theta.txt")
     with file(filename + "_ndw.txt", "r") as infile:
         size = int(infile.readline())
         ndw = []
         for i in xrange(size):
-            sliceShape = tuple(map(int, infile.readline().split()))
-            lines = [infile.readline() for i in xrange(sliceShape[0])]
-            newSlice = np.array(map(lambda s: map(int, s.split()), lines))
-            ndw.append(newSlice)
-    return (phi, theta, ndw)
+            slice_shape = tuple(map(int, infile.readline().split()))
+            lines = [infile.readline() for i in xrange(slice_shape[0])]
+            new_slice = np.array(map(lambda s: map(int, s.split()), lines))
+            ndw.append(new_slice)
+    return phi, theta, ndw
 
 
-def writeDataToFile(filename, phi, theta, ndw):
+def write_data_to_file(filename, phi, theta, ndw):
     np.savetxt(filename + "_phi.txt", phi)
     np.savetxt(filename + "_theta.txt", theta)
     with file(filename + "_ndw.txt", "w") as outfile:
@@ -77,173 +77,169 @@ def writeDataToFile(filename, phi, theta, ndw):
             np.savetxt(outfile, ndwSlice, fmt='%i')
 
 
-def genPhi(wCount, tCount, dirichletFactor):
-    phi = np.empty((wCount, tCount))
-    phi[:, -1] = np.fromfunction(lambda i: 1.0 / (i + 1), (wCount, ), dtype=float) * (
-        0.8 + np.random.random((wCount, )) * 0.4)
-    phi[:, :-1] = mr.dirichlet((-phi[:, -1] + phi[0, -1]) * dirichletFactor, tCount - 1).transpose()
-    cutZeroes(phi)
+def gen_phi(wcount, tcount, dirichlet_factor):
+    phi = np.empty((wcount, tcount))
+    phi[:, -1] = np.fromfunction(lambda i: 1.0 / (i + 1), (wcount, ), dtype=float) * (
+        0.8 + np.random.random((wcount, )) * 0.4)
+    phi[:, :-1] = mr.dirichlet(1 / phi[:, -1] / wcount * dirichlet_factor, tcount - 1).transpose()
+    cut_zeroes(phi)
     phi = np.apply_along_axis(normalized, 0, phi)
     return phi
 
 
-def genTheta(tCount, dCount, dirichletFactor):
-    theta = np.empty((tCount, dCount))
-    theta[-1] = np.random.random((dCount, )) * 3
-    theta[:-1, :] = mr.dirichlet([dirichletFactor] * (tCount - 1), dCount).transpose()
-    cutZeroes(theta)
+def gen_theta(tcount, dcount, dirichlet_factor):
+    theta = np.empty((tcount, dcount))
+    theta[-1] = np.random.random((dcount, )) * 3
+    theta[:-1, :] = mr.dirichlet([dirichlet_factor] * (tcount - 1), dcount).transpose()
+    cut_zeroes(theta)
     theta = np.apply_along_axis(normalized, 0, theta)
     return theta
 
 
-def genCollection(phi, theta):
-    tCount = len(theta)
-    dCount = len(theta[0])
-    nd = mr.randint(600, 1000, dCount)
+def gen_collection(phi, theta):
+    tcount = len(theta)
+    dcount = len(theta[0])
+    nd = mr.randint(600, 1000, dcount)
     pwd = phi.dot(theta)
     ndw = []
-    for d in xrange(dCount):
-        columnD = proundVec(pwd[:, d] * nd[d]).astype(int)
-        columnD = np.dstack((np.arange(len(columnD)), columnD))[0]
-        columnD1 = columnD[:, 1]
-        ndw.append(columnD[np.abs(columnD1) > 0, :])
+    for d in xrange(dcount):
+        columnd = pround(pwd[:, d] * nd[d])
+        columnd_tup = np.dstack((np.arange(len(columnd)), columnd))[0]
+        ndw.append(columnd_tup[columnd > 0, :])
     return ndw
 
 
 def hellinger2(p, q):
     return euclidean(np.sqrt(p), np.sqrt(q)) / np.sqrt(2)
 
-def hellinger2Matrix(a, b):
+
+def hellinger2_matrix(a, b):
     return np.average([hellinger2(a[:, i], b[:, i]) for i in xrange(len(a[0]))])
 
-def normTheta(theta):
-    nTheta = np.empty(theta.shape)
-    for t in xrange(theta.shape[0]):
-        for d in xrange(theta.shape[1]):
-            nTheta[t][d] = theta[t][d] / sum(theta[t, :])
-    return nTheta
-        
-def themeDist(phi0, theta0, phi, theta, i, j):
+
+def theme_dist(phi0, theta0, phi, theta, i, j):
     return hellinger2(np.append(theta0[i], phi0[:, i]), np.append(theta[j], phi[:, j])) ** 2 / 2
 
-def EM(ndw, wCount, tCount, alpha, beta):
-    dCount = len(ndw)
-    print "W =", wCount
-    print "T =", tCount
-    print "D =", dCount
-    theta = np.random.random((tCount, dCount))
-    phi = np.random.random((wCount, tCount))
-    for i in xrange(dCount):
-        theta[:, i] = normalized(theta[:, i])
-    for i in xrange(tCount):
-        phi[:, i] = normalized(phi[:, i])
-    nwt = np.empty((wCount, tCount))
-    ndt = np.empty((dCount, tCount))
-    nd = np.empty(dCount)
-    nt = np.empty(tCount)
-    for d in xrange(dCount):
+
+def EM_algorithm(ndw, wcount, tcount, alpha, beta):
+    dcount = len(ndw)
+    print "W =", wcount
+    print "T =", tcount
+    print "D =", dcount
+    theta = np.random.random((tcount, dcount))
+    phi = np.random.random((wcount, tcount))
+    theta = normalized_matrix_by_columns(theta)
+    phi = normalized_matrix_by_columns(phi)
+    nwt = np.empty((wcount, tcount))
+    ndt = np.empty((dcount, tcount))
+    nd = np.empty(dcount)
+    nt = np.empty(tcount)
+    for d in xrange(dcount):
         nd[d] = ndw[d][:, 1].sum()
 
-    eps = 0.05
-
-    isConverged = False
     step = 0
 
     for i in xrange(50):
         start = time.clock()
-        thetaOld = theta.copy()
-        phiOld = phi.copy()
         nwt.fill(0)
         ndt.fill(0)
 
-        for d in xrange(dCount):
+
+        for d in xrange(dcount):
             for wInd in xrange(len(ndw[d])):
-                w, wVal = (ndw[d][wInd][0], ndw[d][wInd][1])
+                w, w_val = ndw[d][wInd][0], ndw[d][wInd][1]
                 delta = phi[w] * theta[:, d]
-                delta = delta * wVal / sum(delta)
-                nwt[w] += delta
-                ndt[d] += delta
+                delta_sum = np.sum(delta)
+                if delta_sum > 0:
+                    delta = delta * w_val / delta_sum
+                    nwt[w] += delta
+                    ndt[d] += delta
 
-        for w in xrange(wCount):
-            phi[w] = positiveSliceVec(nwt[w] + alpha[w])
-        phi = np.apply_along_axis(normalized, 0, phi)
+        #for w in xrange(wcount):
+        #    phi[w] = positive_slice(nwt[w] + alpha[w])
+        for w in xrange(wcount):
+            phi[w] = positive_slice(nwt[w] - alpha[0] * phi[w] * (np.sum(phi[w]) - phi[w]))
 
-        for t in xrange(tCount):
-            theta[t] = positiveSliceVec(ndt[:, t] + beta[t])
-        theta = np.apply_along_axis(normalized, 0, theta)
+        for t in xrange(tcount):
+            theta[t] = positive_slice(ndt[:, t] + beta[t])
+
+        theta = normalized_matrix_by_columns(theta)
+        phi = normalized_matrix_by_columns(phi)
 
         step += 1
-        nowNorm = hellinger2Matrix(phi, phiOld) + hellinger2Matrix(theta, thetaOld)
+
         finish = time.clock()
-        print "[EM-Algorithm] Step:", step, "Norm:", nowNorm
+        print "[EM-Algorithm] Step:", step
         print "[EM-Algorithm]", "%.2f" % (finish - start), "seconds."
-        isConverged = nowNorm < eps
 
     return phi, theta
 
 
-def generateData(tCount, dCount, wCount, sparsityFactor):
+def generate_data(tcount, dcount, wcount, sparsity_factor):
     start = time.clock()
     print "Generating theta..."
-    theta = genTheta(tCount, dCount, sparsityFactor)
+    theta = gen_theta(tcount, dcount, sparsity_factor)
     finish = time.clock()
     print "Generated in", "%.2f" % (finish - start), "seconds.", "\n"
 
     start = time.clock()
     print "Generating phi..."
-    phi = genPhi(wCount, tCount, sparsityFactor)
+    phi = gen_phi(wcount, tcount, sparsity_factor)
     finish = time.clock()
     print "Generated in", "%.2f" % (finish - start), "seconds.", "\n"
 
     start = time.clock()
     print "Generating collection..."
-    ndw = genCollection(phi, theta)
+    ndw = gen_collection(phi, theta)
     finish = time.clock()
     print "Generated in", "%.2f" % (finish - start), "seconds.", "\n"
-    return (phi, theta, ndw)
+    return phi, theta, ndw
 
-def calculateHellingerDists(correctPhi0, correctTheta0, correctProduct, phi, theta, product):
-    distPhi = hellinger2Matrix(correctPhi0, phi)
-    distTheta = hellinger2Matrix(correctTheta0, theta)
-    distProduct = hellinger2Matrix(correctProduct, product)
-    print "Hellinger distance for phi:", distPhi
-    print "Hellinger distance for theta:", distTheta
-    print "Hellinger distance for product:", distProduct
-    return (distPhi, distTheta, distProduct)
-    
-def constuctCorrectMatrices(phi0, theta0, res):
-    correctPhi0 = np.empty(phi0.shape)
-    correctTheta0 = np.empty(theta0.shape)
-    for e in res:
-        correctPhi0[:, e[1]] = phi0[:, e[0]]
-    for e in res:
-        correctTheta0[e[1]] = theta0[e[0]]
-    correctProduct = correctPhi0.dot(correctTheta0)
-    return (correctPhi0, correctTheta0, correctProduct)
 
-def compareMatrices(phi0, theta0, phi, theta):
-    costMatrix = np.empty((len(theta0), len(theta)))
-    for i in xrange(len(theta0)):
-        for j in xrange(len(theta)):
-            costMatrix[i][j] = themeDist(phi0, theta0, phi, theta, i, j)
+def calculate_hellinger_dists(correct_phi0, correct_theta0, correct_product, phi, theta, product):
+    dist_phi = hellinger2_matrix(correct_phi0, phi)
+    dist_theta = hellinger2_matrix(correct_theta0, theta)
+    dist_product = hellinger2_matrix(correct_product, product)
+    print "Hellinger distance for phi:", dist_phi
+    print "Hellinger distance for theta:", dist_theta
+    print "Hellinger distance for product:", dist_product
+    return dist_phi, dist_theta, dist_product
+
+
+def constuct_correct_matrices(phi0, theta0, res):
+    correct_phi0 = np.empty(phi0.shape)
+    correct_theta0 = np.empty(theta0.shape)
+    for e in res:
+        correct_phi0[:, e[1]] = phi0[:, e[0]]
+    for e in res:
+        correct_theta0[e[1]] = theta0[e[0]]
+    correct_product = correct_phi0.dot(correct_theta0)
+    return correct_phi0, correct_theta0, correct_product
+
+
+def compare_matrices(phi0, theta0, phi, theta):
+    cost_matrix = np.empty((theta0.shape[0], theta.shape[0]))
+    for i in xrange(theta0.shape[0]):
+        for j in xrange(theta.shape[0]):
+            cost_matrix[i][j] = theme_dist(phi0, theta0, phi, theta, i, j)
     print "Hungarian algorithm..."
     start = time.clock()
-    res = linear_assignment(costMatrix)
+    res = linear_assignment(cost_matrix)
     finish = time.clock()
     print "Total time:", "%.2f" % (finish - start), "seconds.", "\n"
     return res
 
-def reconstructSubjects(phi0, theta0, phi, theta):
+
+def reconstruct_subjects(phi0, theta0, phi, theta):
     t = phi.shape[1]
     t0 = phi0.shape[1]
     res = []   
     for i in xrange(t0):
-        distsT = [themeDist(phi0, theta0, phi, theta, i, j) for j in xrange(t)]
-        minInd = np.argmin(distsT)
-        distsT0 = [themeDist(phi0, theta0, phi, theta, j, minInd) for j in xrange(t0)]
-        minInd0 = np.argmin(distsT0)
-        print i, minInd, distsT[minInd], minInd0, distsT0[minInd0]
-        if (minInd0 == i):
-            res.append((minInd, i, distsT[minInd]))
+        dists_t = [theme_dist(phi0, theta0, phi, theta, i, j) for j in xrange(t)]
+        min_ind = np.argmin(dists_t)
+        dists_t0 = [theme_dist(phi0, theta0, phi, theta, j, min_ind) for j in xrange(t0)]
+        min_ind0 = np.argmin(dists_t0)
+        if min_ind0 == i:
+            res.append((min_ind, i, dists_t[min_ind]))
     
     return res
--- main.py
+++ main.py
@@ -6,138 +6,155 @@ import os
 import multiprocessing
 import sys
 from EM import *
+from matplotlib import pyplot as plt
 
-def writePlotData(phi, phi0, theta, theta0, subjects, alpha, beta, id):
+def write_plot_data(phi, phi0, theta, theta0, subjects, alpha, beta, id):
     pfilename = "plot.txt"
 
     rnum = len(subjects)
-    subjectsDist = [subject[2] for subject in subjects]
-    minDist = min(subjectsDist)
-    maxDist = max(subjectsDist)
-    avgDist = np.average(subjectsDist)
+    subjects_dist = np.array([subject[2] for subject in subjects])
+    min_dist = np.min(subjects_dist)
+    max_dist = np.max(subjects_dist)
+    avg_dist = np.average(subjects_dist)
     sparsity = float(np.count_nonzero(phi) + np.count_nonzero(theta)) / (phi.size + theta.size)
     t = len(theta)
     t0 = len(theta0)
     pfile = file(pfilename, "a")
     pfile.write("{0}\t{1}\t{2}\t{3}\t{4}\t{5}\t{6}\t{7}\t{8}\t{9}\n".format(
-        id, rnum, minDist, maxDist, avgDist, sparsity, alpha, beta, t, t0
+        id, rnum, min_dist, max_dist, avg_dist, sparsity, alpha, beta, t, t0
     ))
     pfile.close()
 
 
-def writeSpecialPlotData(subjects, id):
-    splotFile = file("splot.txt", "a")
+def write_special_plot_data(subjects, id):
+    splot_file = file("splot.txt", "a")
     for subject in subjects:
-        splotFile.write("{0}\t{1}\t{2}\t{3}\n".format(id, subject[0], subject[1], subject[2]))
-    splotFile.close()
+        splot_file.write("{0}\t{1}\t{2}\t{3}\n".format(id, subject[0], subject[1], subject[2]))
+    splot_file.close()
 
+def check(tcount, dcount, wcount, alpha_factor, beta_factor, dirichlet_factor):
+    phi, theta, ndw = generate_data(tcount, dcount, wcount, dirichlet_factor)
 
-def check(tCount, dCount, wCount):
-    phi, theta, ndw = generateData(tCount, dCount, wCount, 0.5)
-
-    alpha = [0] * wCount
-    beta = [0] * tCount
+    alpha = np.full((wcount, ), alpha_factor)
+    beta = np.full((tcount, ), beta_factor)
     start = time.clock()
     print "EM-Algorithm..."
-    phi0, theta0 = EM(ndw, wCount, tCount, alpha, beta)
+    phi0, theta0 = EM_algorithm(ndw, wcount, tcount, alpha, beta)
     finish = time.clock()
     print "Total time:", "%.2f" % (finish - start), "seconds.", "\n"
 
     start = time.clock()
     print "Calculating Hellinger distance..."
 
-    nTheta0 = normTheta(theta0)
-    nTheta = normTheta(theta)
-    res = compareMatrices(phi0, nTheta0, phi, nTheta)
-    correctPhi0, correctTheta0, correctProduct = constuctCorrectMatrices(phi0, theta0, res)
-    #subjects = reconstructSubjects(phi0, theta0, phi, theta, res)
-    calculateHellingerDists(correctPhi0, correctTheta0, correctProduct, phi, theta, phi.dot(theta))
+    ntheta0 = normalized_matrix_by_rows(theta0)
+    ntheta = normalized_matrix_by_rows(theta)
+    res = compare_matrices(phi0, ntheta0, phi, ntheta)
+    correct_phi0, correct_theta0, correct_product = constuct_correct_matrices(phi0, theta0, res)
+    calculate_hellinger_dists(correct_phi0, correct_theta0, correct_product, phi, theta, phi.dot(theta))
 
     finish = time.clock()
     print "Calculated in ", "%.2f" % (finish - start), "seconds.", "\n"
 
 
-def checkMoreSubjects(tCount, dCount, wCount, tCountEM, isGeneratingData, isSaveData,
-                      alphaFactor, betaFactor, sparsityFactor, id):
+def check_more_subjects(tcount, dcount, wcount, tcount0, is_generate_data, is_save_data,
+                      alpha_factor, beta_factor, sparsity_factor, id):
     np.random.seed(id)
     if not os.path.exists("log"):
         os.makedirs("log")
-    sys.stdout = open("log/log_{0}.txt".format(id), "w")
-    if isGeneratingData:
-        phi, theta, ndw = generateData(tCount, dCount, wCount, sparsityFactor)
-        if (isSaveData):
-            writeDataToFile("data", phi, theta, ndw)
+    #sys.stdout = open("log/log_{0}.txt".format(id), "w")
+    if is_generate_data:
+        phi, theta, ndw = generate_data(tcount, dcount, wcount, sparsity_factor)
+        if is_save_data:
+            write_data_to_file("data", phi, theta, ndw)
     else:
-        phi, theta, ndw = loadFromFile("data")
+        phi, theta, ndw = load_from_file("data")
 
-    alpha = [alphaFactor] * wCount
-    beta = [betaFactor] * tCount
-    #alpha[-1] = 0.00
-    #beta[-1] = 0.00
+    alpha = np.full((wcount, ), alpha_factor)
+    beta = np.full((tcount, ), beta_factor)
+
     start = time.clock()
     print "EM-Algorithm..."
-    phi0, theta0 = EM(ndw, wCount, tCountEM, alpha, beta)
-    if (isSaveData):
+
+    phi0, theta0 = EM_algorithm(ndw, wcount, tcount0, alpha, beta)
+    if is_save_data:
         np.savetxt("data_phi0.txt", phi0)
         np.savetxt("data_theta0.txt", theta0)
+
     finish = time.clock()
     print "Total time:", "%.2f" % (finish - start), "seconds.", "\n"
 
-    
-    theta = normTheta(theta)
-    theta0 = normTheta(theta0)
-    subjects = reconstructSubjects(phi0, theta0, phi, theta)
-    writePlotData(phi, phi0, theta, theta0, subjects, alphaFactor, betaFactor, id)
-    writeSpecialPlotData(subjects, id)
+    theta = normalized_matrix_by_rows(theta)
+    theta0 = normalized_matrix_by_rows(theta0)
+    subjects = reconstruct_subjects(phi0, theta0, phi, theta)
+
+    if not os.path.exists("dot"):
+        os.makedirs("dot")
+    dots_number = 0
+    for subject in subjects:
+        dots_number += 1
+        np.savetxt("dot/dots" + str(dots_number) + ".txt", np.dstack((phi[:, subject[0]], phi0[:, subject[1]]))[0], delimiter='\t', fmt='%.10f')
+
+    write_plot_data(phi, phi0, theta, theta0, subjects, alpha_factor, beta_factor, id)
+    write_special_plot_data(subjects, id)
 
 
 p = multiprocessing.Pool(processes=3)
 processes = []
 id = int(file("plot.txt").readlines()[-1].split()[0])
-w = 300
-d = 300
-t = 50
+w = 1000
+d = 1000
+t = 250
 t0 = 50
+alpha_factor = 0.0
+beta_factor = 0.0
+dirichlet_factor = 0.01
 
-def genAlphaData(minA, maxA, pointsNum, isGenMatrices):
-    for a in np.linspace(minA, maxA, num=pointsNum):
+def gen_alpha_data(mina, maxa, points_num, is_gen_matrices):
+    global id
+    for a in np.linspace(mina, maxa, num=points_num):
         id += 1
-        processes.append(p.apply_async(checkMoreSubjects, args=(w, d, t, t0, isGenMatrices, False, a, a, 0.01, id)))
+        processes.append(p.apply_async(check_more_subjects,
+                                       args=(t, d, w, t0, is_gen_matrices, False, a, a, dirichlet_factor, id)))
 
-    runProcesses()
+    run_rocesses()
 
 
-def genSparcityData(minD, maxD, pointsNum, isGenMatrices):
-    for d in np.linspace(minD, maxD, num=pointsNum):
+def gen_sparcity_data(mind, maxd, points_num, is_gen_matrices):
+    global id
+    for d in np.linspace(mind, maxd, num=points_num):
         id += 1
         processes.append(
-            p.apply_async(checkMoreSubjects, args=(w, d, t, t0, isGenMatrices, False, -0.02, -0.02, d, id)))
+            p.apply_async(check_more_subjects,
+                          args=(t, d, w, t0, is_gen_matrices, False, alpha_factor, beta_factor, d, id)))
 
-    runProcesses()
+    run_rocesses()
 
 
-def genTData(minT, maxT, stepT, minT0, maxT0, stepT0, isGenMatrices):
-    for t0 in xrange(minT0, maxT0, stepT0):
-        for t in xrange(minT, maxT, stepT):
+def gen_tdata(mint, maxt, stept, mint0, maxt0, stept0, is_gen_matrices):
+    global id
+    for t0 in xrange(mint0, maxt0, stept0):
+        for t in xrange(mint, maxt, stept):
             id += 1
             processes.append(
-                p.apply_async(checkMoreSubjects, args=(t, w, d, t0, isGenMatrices, False, -0.02, -0.02, 0.01, id)))
+                p.apply_async(check_more_subjects,
+                              args=(t, d, w, t0, is_gen_matrices, False, alpha_factor, beta_factor, dirichlet_factor, id)))
 
-    runProcesses()
+    run_rocesses()
 
 
-def genSpecialData(pointsNum):
+def gen_special_data(points_num):
     global id
     id += 1
-    checkMoreSubjects(t, w, d, t0, True, True, -0.02, -0.02, 0.01, id)
-    for i in xrange(pointsNum):
+    check_more_subjects(t, w, d, t0, True, True, 0, 0, 0.01, id)
+    for i in xrange(points_num):
         id += 1
-        processes.append(p.apply_async(checkMoreSubjects, args=(t, w, d, t0, False, False, -0.02, -0.02, 0.01, id)))
+        processes.append(p.apply_async(check_more_subjects,
+                                       args=(t, w, d, t0, False, False, alpha_factor, beta_factor, dirichlet_factor, id)))
 
-    runProcesses()
+    run_rocesses()
 
 
-def runProcesses():
+def run_rocesses():
     global processes
     for p in processes:
         try:
@@ -149,4 +166,5 @@ def runProcesses():
 
     processes = []
 
-checkMoreSubjects(t, w, d, t0, True, False, 0, 0, 0.5, id)
+#check(t, d, w, alpha_factor, beta_factor, dirichlet_factor)
+check_more_subjects(t, d, w, t0, True, False, 7000, 0, 0.01, id)
